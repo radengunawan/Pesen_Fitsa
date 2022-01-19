07abba1480e54da876fd88642d925abce09b5f40
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-19 13:47:34 +0800

    Add project files.

---
+++ Pesen_Fitsa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pesen_Fitsa
+{
+    class Nanas
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First attempt");
+        }
+    }
+}
