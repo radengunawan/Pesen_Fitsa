c22f94c1bd208281abd593973e0b69e1ba818ddd
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-19 13:51:03 +0800

    second progress

--- Pesen_Fitsa/Program.cs
+++ Pesen_Fitsa/Program.cs
@@ -4,9 +4,15 @@ namespace Pesen_Fitsa
 {
     class Nanas
     {
+        const string d = "delivery.txt";
+        enum Fargo { North, South, East, West, Flamingo }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("First attempt");
+        
+            
+
+
         }
     }
 }
